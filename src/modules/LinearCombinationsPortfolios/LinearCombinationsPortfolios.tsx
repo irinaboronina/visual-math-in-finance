06635d24394f9d5683c6a7f674df063fcafc6e17
irinaboronina <irina.a.boronina@gmail.com>
2026-07-19 13:32:53 -0400

    Update LinearCombinationsPortfolios.tsx
    Fixed issue in one module

--- src/modules/LinearCombinationsPortfolios/LinearCombinationsPortfolios.tsx
+++ src/modules/LinearCombinationsPortfolios/LinearCombinationsPortfolios.tsx
@@ -1,8 +1,9 @@
 function LinearCombinationsPortfolios() {
-  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  rtfoli  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retuctio  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retuffi  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retff  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retu  retncti  retu  retu  retu
-              Ran          com              Ran 
-exexet defexexet defexexet dEOex
-cacacacacacacacacacacacacacacacacacacacacacacacaOFcacacacacacacacacacao(cacacacacacacacacacacacacacacacacacacaoon</div>;
+  return (
+    <div>
+      <h2>Linear Combinations & Portfolios coming soon</h2>
+    </div>
+  );
 }
 
-export default MonteCarexport default MonteCarexport default MonteCarexport default MonteCareEOF'export default MonteCarexport def rexport default MonteCarexport default MonteCarexport defrt default ExponentialGrowth;
+export default LinearCombinationsPortfolios;
